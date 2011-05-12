0b14107d11502bf2ea4502538a10fd2c643deafc
Jason Laster <jason.laster.11@gmail.com>
2011-05-11 21:00:32 -0400

    more changes to grpahics

--- src/Graphics.c
+++ src/Graphics.c
@@ -10,8 +10,7 @@
 static float ang = 5.;
 static int duration[NUM_MIDI];
 
-static gboolean
-expose (GtkWidget *da, GdkEventExpose *event, gpointer user_data) {
+static gboolean expose (GtkWidget *da, GdkEventExpose *event, gpointer user_data) {
 	GdkGLContext *glcontext = gtk_widget_get_gl_context (da);
 	GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (da);
 
@@ -68,14 +67,12 @@ static gboolean configure (GtkWidget *da, GdkEventConfigure *event, gpointer use
 	}
 
 	glLoadIdentity();
-	glViewport (5, 5, da->allocation.width, da->allocation.height);
-	glOrtho (-10,10,-10,10,-20050,10000);
-	glEnable (GL_BLEND);
-	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	glScalef (8., 8., 10.);
-	
-	gdk_gl_drawable_gl_end (gldrawable);
+	glViewport(5, 5, da->allocation.width, da->allocation.height);
+	glOrtho(-10,10,-10,10,-20050,10000);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glScalef(8., 8., 10.);
+	gdk_gl_drawable_gl_end(gldrawable);
 
 	return TRUE;
 }
@@ -91,23 +88,18 @@ gboolean rotate (gpointer user_data) {
 void set_up_graphics(GtkWidget *da) {
 
 	GdkGLConfig *glconfig;
-
+	glconfig = gdk_gl_config_new_by_mode(GDK_GL_MODE_RGB | GDK_GL_MODE_DEPTH | GDK_GL_MODE_DOUBLE);
   set_zero();
 
-	glconfig = gdk_gl_config_new_by_mode (
-			GDK_GL_MODE_RGB |
-			GDK_GL_MODE_DEPTH |
-			GDK_GL_MODE_DOUBLE);
-
 	if (!glconfig) {
-		g_assert_not_reached ();
+		g_assert_not_reached();
 	}
 
-	if (!gtk_widget_set_gl_capability (da, glconfig, NULL, TRUE, GDK_GL_RGBA_TYPE)) {
-		g_assert_not_reached ();
+	if (!gtk_widget_set_gl_capability(da, glconfig, NULL, TRUE, GDK_GL_RGBA_TYPE)) {
+		g_assert_not_reached();
 	}
 
-	g_signal_connect (da, "configure-event", G_CALLBACK (configure), NULL);
-	g_signal_connect (da, "expose-event", G_CALLBACK (expose), NULL);
+	g_signal_connect(da, "configure-event", G_CALLBACK (configure), NULL);
+	g_signal_connect(da, "expose-event", G_CALLBACK (expose), NULL);
 }
 
--- src/Graphics_Driver.c
+++ src/Graphics_Driver.c
@@ -9,12 +9,12 @@ int main(int argc, char **argv) {
 	gtk_init (&argc, &argv);
 	gtk_gl_init (&argc, &argv);
 
-	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_default_size(GTK_WINDOW (window), 800, 600);
-	da = gtk_drawing_area_new ();
+	da = gtk_drawing_area_new();
 
 	gtk_container_add(GTK_CONTAINER (window), da);
-	g_signal_connect_swapped (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+	g_signal_connect_swapped(window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
 	gtk_widget_set_events(da, GDK_EXPOSURE_MASK);
   set_up_graphics(da);
